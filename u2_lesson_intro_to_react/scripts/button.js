da4d2592d3d43d2288d1e9aa3ecda0bafc98dd1f
iMac1 <irina.macovei@yahoo.fr>
2024-12-18 00:30:06 +0200

    l2

--- u2_lesson_intro_to_react/scripts/button.js
+++ u2_lesson_intro_to_react/scripts/button.js
@@ -1,1 +1,48 @@
 const container = document.getElementById('root')
+
+// class MyButton extends React.Component {
+    // constructor () {
+    //     super()
+    //     this.state = {
+    //         counter: 0
+    //     }
+    //     this.incrementCounter = this.incrementCounter.bind(this)
+    // }
+// incrementCounter () {
+//     this.setState({counter: this.state.counter + 1})
+// }
+
+// render () {
+//     return  React.createElement('button', { onClick: this.incrementCounter},
+//         this.state.counter
+//     )
+// }
+
+
+
+// }
+
+function Button() {
+    const [counter, updateCounter] = React.useState(0)
+    const handleCounterUpdate = () => {
+        updateCounter(counter + 1)
+    }
+    // return React.createElement(
+    //     'button',
+    //     {
+    //         onClick: handleCounterUpdate,
+    //         className: 'my-button'
+    //     },
+    //     counter
+    // )
+
+    return (
+        <button className="babel-button" onClick={handleCounterUpdate}>
+          {counter}
+        </button>
+      )
+}
+
+
+const root = ReactDOM.createRoot(container)
+root.render(<Button />)
