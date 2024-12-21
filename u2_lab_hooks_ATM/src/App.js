10f1dfcd1697e9fef184bb7f395ad2a06e750cd9
iMac1 <irina.macovei@yahoo.fr>
2024-12-21 16:42:52 +0200

    updated withdraw

--- u2_lab_hooks_ATM/src/App.js
+++ u2_lab_hooks_ATM/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       </div>
       <div className="bank">
         <Account name="Checking" />
+    
       </div>
     </div>
   )
--- u2_lab_hooks_ATM/src/components/Account.js
+++ u2_lab_hooks_ATM/src/components/Account.js
@@ -1,49 +1,66 @@
-import { useState } from 'react'
-import '../styles/App.css'
+import { useState } from "react";
+import "../styles/App.css";
 
 const Account = (props) => {
-  let [amount, setAmount] = useState("")
-  let [balance, setBalance] = useState(0)
+	let [amount, setAmount] = useState("");
+	let [balance, setBalance] = useState(0);
 
-  const handleClick = (event) => {
-    event.preventDefault()
-    if (isNaN(amount)) {
-      console.log('Not a number')
-    } else {
-      setBalance(balance + Number(amount))
-    }
-    setAmount(0)
-  }
 
-  let balanceClass = 'balance'
-  if (balance <= 0) {
-    balanceClass += ' zero'
-  }
+	const handleWithdrawClick = (event) => {
+		event.preventDefault();
+		if (isNaN(amount)) {
+			console.log("Not a number");
+		} else {
+			setBalance(balance - Number(amount));
+		}
+		setAmount(0);
+	};
 
-  return (
-    <div className="account">
-      <h2>{props.name}</h2>
-      <div className={balanceClass}>${balance}</div>
-      <br />
-      <div>
-        <input
-          className="input"
-          type="text"
-          placeholder="amount"
-          value={amount}
-          onChange={(event) => setAmount(event.target.value)}
-        />
-        <br />
-        <br />
-        <input
-          className="btn"
-          type="submit"
-          value="Deposit"
-          onClick={handleClick}
-        />
-      </div>
-    </div>
-  )
-}
+	const handleClick = (event) => {
+		event.preventDefault();
+		if (isNaN(amount)) {
+			console.log("Not a number");
+		} else {
+			setBalance(balance + Number(amount));
+		}
+		setAmount(0);
+	};
 
-export default Account
+	let balanceClass = "balance";
+	if (balance <= 0) {
+		balanceClass += " zero";
+	}
+
+	return (
+		<div className="account">
+			<h2>{props.name}</h2>
+			<div className={balanceClass}>${balance}</div>
+			<br />
+			<div>
+				<input
+					className="input"
+					type="text"
+					placeholder="amount"
+					value={amount}
+					onChange={(event) => setAmount(event.target.value)}
+				/>
+				<br />
+				<br />
+				<input
+					className="btn"
+					type="submit"
+					value="Deposit"
+					onClick={handleClick}
+				/>
+				<input
+					className="btn"
+					type="submit"
+					value="Withdraw"
+					onClick={handleWithdrawClick}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Account;
