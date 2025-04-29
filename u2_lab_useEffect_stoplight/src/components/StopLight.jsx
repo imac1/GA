6eddb4263afc9277568b34d763d794f0e5e10647
iMac1 <irina.macovei@yahoo.fr>
2025-04-29 22:04:32 +0300

    toggleRacing implemented

--- u2_lab_useEffect_stoplight/src/components/StopLight.jsx
+++ u2_lab_useEffect_stoplight/src/components/StopLight.jsx
@@ -1,25 +1,36 @@
-const StopLight = (props) => {
+const StopLight = ({ lights, setLights }) => {
+	console.log(lights)
+	return (
+		<div className="action-area">
+			<h1>Stoplight useEffect</h1>
+			<div className="stoplight-container">
+				<div className="stoplight">
+					<div className={lights.red ? 'red': undefined}></div>
+					<div className={lights.yellow ? 'yellow': undefined}></div>
+					<div className={lights.green? 'green': undefined}></div>
+				</div>
+				<div>
+					<button
+						className="countdown"
+						// onCLick here
+						onClick={() => {
+							if (lights.red && lights.yellow && lights.green) {
+								setLights({ red: false, yellow: false, green: false });
+							} else if (lights.red && lights.yellow) {
+								setLights({ green: true, red: false, yellow: false });
+							} else if (lights.red) {
+								setLights({ red: true, yellow: true, green: true });
+							} else {
+								setLights({ red: true, yellow: false, green: false });
+							}
+						}}	 	 					
+					>
+						{lights.red && lights.yellow && lights.green ? "Reset" : "Countdown"}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div className="action-area">
-      <h1>Stoplight useEffect</h1>
-      <div className="stoplight-container">
-        <div className="stoplight">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div>
-          <button
-            className="countdown"
-            // onCLick here
-          >
-          Countdown
-          </button>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default StopLight
+export default StopLight;
